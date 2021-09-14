eef02c4dd147ef09690ef8c9d12604738cdda98b
wynnewuu <wynnewuu@gmail.com>
2021-09-14 08:48:18 -0400

    upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+    	int j=1;
+        for(int i=1; i<=9; i++)
+        {
+        for(j=1; j<=9; j++ )
+        System.out.println(i + "*" + j + "=" + (i*j));  
+        }
+    }
+    }
